7f0c3a7ae8f39c6c8b974f7c4cd3f2d5e5ba81f9
ashikurrafi <ashikurrafi@gmail.com>
2022-01-30 19:32:43 +0600

    Bug fixed

--- Finding largest number from three classes/Finding largest number from three classes.cpp
+++ Finding largest number from three classes/Finding largest number from three classes.cpp
@@ -1,34 +1,33 @@
 #include <iostream>
 using namespace std;
-
 class Class_Three;
-class class_Two;
+class Class_Two;
 class Class_One
 {
 public:
-	Class_One();
-	Class_One(int n1);
-	void set_data(int value);
-	friend void find_greatest_number(Class_One, Class_Two, Class_Three);
-	~Class_One();
+    Class_One();
+    Class_One(int n);
+    void set_data(int value);
+    friend void find_greatest_number(Class_One, Class_Two, Class_Three);
+    ~Class_One();
 
 private:
-	double Number1;
+    int Number1;
 };
 
 Class_One::Class_One()
 {
-	Number1 = 0;
+    Number1 = 0;
 }
 
-Class_One::Class_One(int n1)
+Class_One::Class_One(int n)
 {
-	Number1 = n1;
+    Number1 = n;
 }
 
 void Class_One::set_data(int value)
 {
-	Number1 = value;
+    Number1 = value;
 }
 
 Class_One::~Class_One()
@@ -38,29 +37,29 @@ Class_One::~Class_One()
 class Class_Two
 {
 public:
-	Class_Two();
-	Class_Two(int n2);
-	void set_data(int value);
-	friend void find_greatest_number(Class_One, Class_Two, Class_Three);
-	~Class_Two();
+    Class_Two();
+    Class_Two(int n);
+    void set_data(int value);
+    friend void find_greatest_number(Class_One, Class_Two, Class_Three);
+    ~Class_Two();
 
 private:
-	int Number2;
+    int Number2;
 };
 
 Class_Two::Class_Two()
 {
-	Number2 = 0;
+    Number2 = 0;
 }
 
-Class_Two::Class_Two(int n2)
+Class_Two::Class_Two(int n)
 {
-	Number2 = n2;
+    Number2 = n;
 }
 
 void Class_Two::set_data(int value)
 {
-	Number2 = value;
+    Number2 = value;
 }
 
 Class_Two::~Class_Two()
@@ -70,73 +69,71 @@ Class_Two::~Class_Two()
 class Class_Three
 {
 public:
-	Class_Three();
-	Class_Three(int n3);
-	void set_data(int value);
-	friend void find_greatest_number(Class_One, Class_Two, Class_Three);
-	~Class_Three();
+    Class_Three();
+    Class_Three(int n);
+    void set_data(int value);
+    friend void find_greatest_number(Class_One, Class_Two, Class_Three);
+    ~Class_Three();
 
 private:
-	int Number3;
+    int Number3;
 };
 
 Class_Three::Class_Three()
 {
-	Number3 = 0;
+    Number3 = 0;
 }
 
-Class_Three::Class_Three(int n3)
+Class_Three::Class_Three(int n)
 {
-	Number3 = n3;
+    Number3 = n;
 }
 
 void Class_Three::set_data(int value)
 {
-	Number3 = value;
+    Number3 = value;
 }
 
 Class_Three::~Class_Three()
 {
 }
 
-
 void find_greatest_number(Class_One obj1, Class_Two obj2, Class_Three obj3)
 {
-	if (obj1.Number1 > obj2.Number2 && obj1.Number1 > obj3.Number3)
-	{
-		cout << "Largest number is " << obj1.Number1 << endl;
-	}
-	else if (obj2.Number2 > obj1.Number1 && obj2.Number2 > obj3.Number3)
-	{
-		cout << "Largest number is " << obj2.Number2 << endl;
-	}
-	else
-	{
-		cout << "Largest number is " << obj3.Number3 << endl;
-	}
-
+    if (obj1.Number1 > obj2.Number2 && obj1.Number1 > obj3.Number3)
+    {
+        cout << "Greatest number is " << obj1.Number1 << endl;
+    }
+    else if (obj2.Number2 > obj1.Number1 && obj2.Number2 > obj3.Number3)
+    {
+        cout << "Greatest number is " << obj2.Number2 << endl;
+    }
+    else
+    {
+        cout << "Greatest number is " << obj3.Number3 << endl;
+    }
 }
 
 int main()
 {
-	Class_One n1;
-	Class_Two n2;
-	Class_Three n3;
-	int num1;
-	int num2;
-	int num3;
+    Class_One n1;
+    Class_Two n2;
+    Class_Three n3;
+    int num1;
+    int num2;
+    int num3;
 
-	cout << "Enter a number: ";
-	cin >> num1;
-	cout << "Enter another number: ";
-	cin >> num2;
-	cout << "Enter another number: ";
-	cin >> num3;
+    cout << "Enter a number: ";
+    cin >> num1;
+    cout << "Enter another number: ";
+    cin >> num2;
+    cout << "Enter another number: ";
+    cin >> num3;
 
-	n1.set_data(num1);
-	n2.set_data(num2);
-	n3.set_data(num3);
-	find_greatest_number(n1, n2, n3);
+    n1.set_data(num1);
+    n2.set_data(num2);
+    n3.set_data(num3);
+    find_greatest_number(n1, n2, n3);
 
-	return 0;
+    return 0;
 }
